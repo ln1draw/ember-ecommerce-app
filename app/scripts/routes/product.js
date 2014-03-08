acaa14b649125b4200e7c93d63ea7aac2c898823
Bookis Smuin <bookis.smuin@gmail.com>
2014-03-07 23:31:05 -0800

    adds notes

--- app/scripts/routes/product.js
+++ app/scripts/routes/product.js
@@ -1,6 +1,7 @@
 App.ProductRoute = Ember.Route.extend({
   actions: {
     addToCart: function (product) {
+      // After setting up the controller model I can do this
       cart = this.controllerFor("cart").get("model")
       item = this.store.createRecord("item", {cart: cart, product: product});
       cart.get("items").then(function (items) {
@@ -10,6 +11,8 @@ App.ProductRoute = Ember.Route.extend({
     }
   },
   setupController: function (controller, model) {
+    // I have to set the controllers model for a model that I want to use
+    // in the setup. Then I can call that controllers model in an action.
     this.controllerFor("cart").set("model", this.store.find("cart", 1));
     controller.set("model", model)
   },
