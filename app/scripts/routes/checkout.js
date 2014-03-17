40b8a1d9229c21d88b77f89a4ad87b93736c66ae
shanghaiellen <shanghaiellen@gmail.com>
2014-03-17 10:33:38 -0700

    trying to get the purchase function to work correctly

--- app/scripts/routes/checkout.js
+++ app/scripts/routes/checkout.js
@@ -1,15 +1,43 @@
 App.CheckoutRoute = Ember.Route.extend({
   actions:{
-    purchase: function(purchase) {
-      purchase.save().then(function(purchase){
-        console.log(purchase);
-      })
+    purchase: function(proxy){
+      // this should eventually be a real method that taps the rails API
+      var sendPurchase = true;
+
+      // the if statement should actually check the content returned
+      if (sendPurchase){
+
+        var purchase = this.store.createRecord("purchase", proxy);
+        var self = this;
+
+        purchase.save().then(function (purchase) {
+          self.transitionTo("purchase", purchase.get("id"));
+        }, function (error) {
+          purchase.deleteRecord();
+          alert(error.responseText)
+        });
+        
+      } else {
+        // this should pull out the actual error returned from the rails API
+        alert(sendPurchase.responseText);
+      }
     }
+
+    // commenting out to test something
+    // purchase: function(purchase) {
+    //   purchase.save().then(function(purchase){
+    //     console.log(purchase);
+    //   })
+    // }
   },
 
   model: function(){
-    window.elistupidstore = this.store
-    var purchase = this.store.createRecord('purchase', {});
-    return purchase;
+    // eli added this to allow himself to access the store in the console
+    // window.elistupidstore = this.store
+
+    // commenting the rest of this out to make sure that everything works!
+    // var purchase = this.store.createRecord('purchase', {});
+    // return purchase;
+    return {};
   }
 })
