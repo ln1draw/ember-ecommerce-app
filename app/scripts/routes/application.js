d20286afa039ea7ce647cebec27c1e93f575bc20
shanghaiellen <shanghaiellen@gmail.com>
2014-03-18 14:16:52 -0700

    hackity hack

--- app/scripts/routes/application.js
+++ app/scripts/routes/application.js
@@ -15,11 +15,15 @@ App.ApplicationRoute = Ember.Route.extend({
   model: function(){
     if (typeof(localStorage.cartId) === "undefined"){
       var cart = this.store.createRecord("cart");
-      cart.save();
-      // added .then to get the return to happen after the cart has been created
-      localStorage.cartId = cart.get("id").then(function(){
+      cart.save().then(function(){
+        // added .then to get the return to happen after the cart has been created
+        localStorage.cartId = cart.get("id");
+        localStorage.save().then(function(){}, function(){
+          return  this.store.find('cart', localStorage.cartId);
+        });
+      });
+    } else {
       return  this.store.find('cart', localStorage.cartId);
-    })
     }
   }
 });
