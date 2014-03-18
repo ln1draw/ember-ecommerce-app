0fa3b711f5b523672862111302e1540b2cdd9faa
shanghaiellen <shanghaiellen@gmail.com>
2014-03-18 14:26:50 -0700

    OMG CART WORKS

--- app/scripts/routes/application.js
+++ app/scripts/routes/application.js
@@ -1,29 +1,14 @@
 App.ApplicationRoute = Ember.Route.extend({
-  // beforeModel: function(){
-  //   if (typeof(localStorage.cartId) === "undefined"){
-  //     var cart = this.store.createRecord("cart");
-  //     cart.save();
-  //     localStorage.cartId = cart.get("id");
-  //   }
-  // }
-  // model: function(){
-  //   return this.store.find("cart", localStorage.cartId);
-  // }
-
-  // before model wasn't firing before the model, so I added the promise object below
 
   model: function(){
     if (typeof(localStorage.cartId) === "undefined"){
       var cart = this.store.createRecord("cart");
-      cart.save().then(function(){
-        // added .then to get the return to happen after the cart has been created
-        localStorage.cartId = cart.get("id");
-        localStorage.save().then(function(){}, function(){
-          return  this.store.find('cart', localStorage.cartId);
-        });
+      return cart.save().then(function(cartObject){
+        localStorage.cartId = cartObject.get('id');
+        return cartObject;
       });
     } else {
-      return  this.store.find('cart', localStorage.cartId);
+      return this.store.find('cart', localStorage.cartId);
     }
   }
 });
