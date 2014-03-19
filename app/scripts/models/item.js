e6291845382e3fea5f15ccdb73e3c077a940764c
shanghaiellen <shanghaiellen@gmail.com>
2014-03-19 10:10:14 -0700

    changes item attributes back to product and cart from product_id and cart_id to help with Ember RESTful routes

--- app/scripts/models/item.js
+++ app/scripts/models/item.js
@@ -1,7 +1,8 @@
 App.Item = DS.Model.extend({
   quantity: DS.attr("number"),
   item_price: DS.attr("number"),
-  product_id: DS.belongsTo('product', {async: true}),
+  product: DS.belongsTo('product', {async: true}),
+  cart: DS.belongsTo('cart', {async: true}), 
   subtotal: function(){
     return this.get('quantity') * this.get('item_price');
   }.property('quantity', 'item_price')
