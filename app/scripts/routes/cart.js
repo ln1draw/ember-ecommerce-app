9a3ffa65236a657dc63e294a09a65a7dd42e44a0
shanghaiellen <shanghaiellen@gmail.com>
2014-03-19 12:08:59 -0700

    increase quantity works

--- app/scripts/routes/cart.js
+++ app/scripts/routes/cart.js
@@ -1,13 +1,16 @@
 App.CartRoute = Ember.Route.extend({
   actions: {
     more: function(item) {
-      item.incrementProperty('quantity')
+      item.incrementProperty('quantity');
+      item.save();
     },
 
     less: function(item) {
       this.store.find("cart").then(function (cart){
         if (item.get('quantity') > 1){
-          item.decrementProperty('quantity');
+          item.decrementProperty('quantity').then(function (cart){
+            item.save();
+          })
         } else {
           cart.get("items").then(function(items){
             items.removeObject(item);
